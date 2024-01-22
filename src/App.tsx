36ebc43b0fdbf64201f0141c573e6f33af339f9e
Wira Zeta Prakosa <dontknow1213@gmail.com>
2024-01-22 14:59:12 +0700

    Second start

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react';
 import './App.css'
-
-import Home from './pages/home';
-
+import Navbar from './components/navbar'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
+      <Navbar/>
     </>
   )
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,22 +1,43 @@
+import { useState } from "react";
+import { Link } from "react-scroll";
+import { FaTimes } from 'react-icons/fa';
+import { CiMenuFries } from 'react-icons/ci';
 import navItems from "./items"
+import ProfilePicture from '../icons/ProfilePicture.jpg';
 
 function Navbar() {
+
+    const [click, setClick] = useState(false);
+    const content = <></>
+    const handleClick = () => {
+        setClick(!click);
+    }
+
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <nav className="bg-white border-gray-200 dark:bg-gray-300 dark:border-gray-700">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <div className="hidden w-full md:block md: w-auto">
-                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <div className="flex rounded-lg size-10">
+                    <img src={ProfilePicture}></img>
+                </div>
+                <div className="max-md:hidden">
+                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-300 md:dark:bg-gray-300 dark:border-gray-300">
                         {
                             navItems.map((item) => {
                                 return (
-                                    <li>
-                                        <a href={item.href} className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">{item.name}</a>
-                                    </li>
+                                    <Link spy={true} smooth={true} to={item.name}>
+                                        <li className="text-black hover:text-blue-700 transition border-b-2 border-gray-300 hover:border-blue-700 cursor-pointer">{item.name}</li>
+                                    </Link>
                                 )
                             })
                         }
                     </ul>
+                    <div>
+                        {click && content}
+                    </div>
                 </div>
+                <button className="block sm:hidden transition" onClick={handleClick}>
+                    {click ? <FaTimes /> : <CiMenuFries />}
+                </button>
             </div>
         </nav>
     )
