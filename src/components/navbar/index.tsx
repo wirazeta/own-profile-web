8daf19ab122be0fd9290fc8540c56d3d0802f910
Wira Zeta Prakosa <dontknow1213@gmail.com>
2024-01-23 12:26:37 +0700

    Edit Navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaTimes, FaBars } from 'react-icons/fa';
-import { CiMenuFries } from 'react-icons/ci';
 import navItems from "./items"
 import ProfilePicture from '../icons/ProfilePicture.jpg';
 
